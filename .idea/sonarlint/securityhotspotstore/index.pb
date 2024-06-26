da38b42f0e6b30308eab31455353b327d65a1135
Anthony <ravija.20200624@iit.ac.lk>
2024-06-26 08:49:05 +0530

    File read

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,9 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+=
+src/Game.java,b\8\b8792f7569afb04e93ae07d923c5005933c47d72
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,159 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static Scanner scanner = new Scanner(System.in);
+    static String[][] mazeArray;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.print("""
+                1. Demo
+                2. Example File
+                Select one:\s""");
+        int opt = scanner.nextInt();
+
+        switch (opt) {
+            case 1:
+                System.out.print("=== Demo ===");
+                System.out.println();
+                demo();
+                break;
+            case 2:
+                System.out.print("=== From Example File ===");
+                System.out.println();
+                SwingUtilities.invokeLater(() -> {
+                    try {
+                        chooseFile();
+                    } catch (InterruptedException | FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                break;
+            default:
+                System.out.println("Invalid Option");
+        }
     }
+
+    /// Demo file
+    static void demo() {
+        File myFile = new File("./maze_folder/examples/maze10_1.txt");
+        try {
+            int lines = 0;
+            int columns = 0;
+            scanner = new Scanner(myFile);
+            while (scanner.hasNextLine()) {
+                lines++;
+                String line = scanner.nextLine();
+                if (line.length() > columns) {
+                    columns = line.length();
+                }
+            }
+
+            scanner.close();
+
+            // Initialize mazeArray
+            mazeArray = new String[lines][columns];
+
+            scanner = new Scanner(myFile);
+            int lineIndex = 0;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                for (int i = 0; i < line.length(); i++) {
+                    mazeArray[lineIndex][i] = String.valueOf(line.charAt(i));
+                }
+                lineIndex++;
+            }
+            scanner.close();
+
+
+//             Print the mazeArray (for verification)
+            for (int i = 0; i < mazeArray.length; i++) {
+                for (int j = 0; j < mazeArray[i].length; j++) {
+                    System.out.print(mazeArray[i][j]);
+                }
+                System.out.println();
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /// Choose the file
+    static void chooseFile() throws InterruptedException, FileNotFoundException {
+
+        // Set the initial directory to the specific folder
+        File initialDirectory = new File("./maze_folder");
+
+//        if (initialDirectory.exists() && initialDirectory.isDirectory()) {
+//            fileChooser.setCurrentDirectory(initialDirectory);
+//        } else {
+//            System.out.println("Directory does not exist.");
+//            return;
+//        }
+
+        JFrame myWindow = new JFrame("Maze Solver");
+        myWindow.setSize(500,400);
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setCurrentDirectory(initialDirectory);
+        int returnVal = jFileChooser.showOpenDialog(myWindow);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jFileChooser.getSelectedFile();
+            readAndPrintMazeFile(selectedFile);
+        } else {
+            System.out.println("File selection cancelled.");
+        }
+    }
+
+    /// Read and print the maze file
+    static void readAndPrintMazeFile(File file) {
+        try {
+            int lines = 0;
+            int columns = 0;
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                lines++;
+                String line = scanner.nextLine();
+                if (line.length() > columns) {
+                    columns = line.length();
+                }
+            }
+
+            scanner.close();
+
+            // Initialize mazeArray
+            mazeArray = new String[lines][columns];
+
+            scanner = new Scanner(file);
+            int lineIndex = 0;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                for (int i = 0; i < line.length(); i++) {
+                    mazeArray[lineIndex][i] = String.valueOf(line.charAt(i));
+                }
+                lineIndex++;
+            }
+            scanner.close();
+
+            // Print the mazeArray (for verification)
+            for (int i = 0; i < mazeArray.length; i++) {
+                for (int j = 0; j < mazeArray[i].length; j++) {
+                    System.out.print(mazeArray[i][j]);
+                }
+                System.out.println();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
